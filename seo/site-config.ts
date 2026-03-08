756003a85f6db59ecdf24f1e6a4ca12fdb878c8b
Thembinkosi Klein <thembinkosiklein@users.noreply.github.com>
2026-03-08 21:49:08 +0200

    Update site-config.ts

--- seo/site-config.ts
+++ seo/site-config.ts
@@ -1,7 +1,8 @@
 export const siteConfig = {
   name: "ITIZ",
   baseUrl: "https://www.itiz.com",
-  title: "!TiZ - Responsive HTML 5 template",
-  description: "Awesome App for Your Modern Lifestyle.",
+  title: "!TiZ - Request Your Superstar, To Your City.",
+  description:
+    "The first platform connecting fans with Superstars. Music, Movies, Theatre, Sports, Stand-Up Comedy and more.",
   defaultOgImage: "/img/slider-moc-2.png",
 } as const;
